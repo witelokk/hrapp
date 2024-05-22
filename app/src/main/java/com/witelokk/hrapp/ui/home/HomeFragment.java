daed41611e95b805a5fd7e23405f1b037e6b14ee
witelokk <witelokk@gmail.com>
2024-05-22 17:20:55 +0300

    add logout dialog

--- app/src/main/java/com/witelokk/hrapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/witelokk/hrapp/ui/home/HomeFragment.java
@@ -10,6 +10,7 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.MenuHost;
 import androidx.core.view.MenuProvider;
@@ -17,8 +18,11 @@ import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.witelokk.hrapp.R;
 import com.witelokk.hrapp.api.model.Company;
+import com.witelokk.hrapp.databinding.DialogDeleteCompanyBinding;
+import com.witelokk.hrapp.databinding.DialogLogoutBinding;
 import com.witelokk.hrapp.databinding.FragmentHomeBinding;
 import com.witelokk.hrapp.ui.BaseFragment;
 
@@ -56,8 +60,7 @@ public class HomeFragment extends BaseFragment<HomeViewModel> {
             @Override
             public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
                 if (menuItem.getItemId() == R.id.menu_logout) {
-                    viewModel.logout();
-                    getNavController().navigate(R.id.action_to_loginFragment);
+                    showLogoutDialog();
                 }
                 return false;
             }
@@ -88,5 +91,21 @@ public class HomeFragment extends BaseFragment<HomeViewModel> {
     private void navigateToCompanyFragment(Company company) {
         HomeFragmentDirections.ActionHomeFragmentToCompanyFragment action = HomeFragmentDirections.actionHomeFragmentToCompanyFragment(company.getId(), company.getName());
         getNavController().navigate(action);
+    }
+
+    void showLogoutDialog() {
+        MaterialAlertDialogBuilder dialogBuilder = new MaterialAlertDialogBuilder(requireContext());
+        DialogLogoutBinding dialogBinding = DialogLogoutBinding.inflate(getLayoutInflater());
+        dialogBuilder.setView(dialogBinding.getRoot());
+
+        dialogBuilder.setTitle(R.string.logout);
+        dialogBuilder.setPositiveButton(R.string.to_logout, (dialog, whichButton) -> {
+            viewModel.logout();
+            getNavController().navigate(R.id.action_to_loginFragment);
+        });
+        dialogBuilder.setNegativeButton(R.string.cancel, (dialog, whichButton) -> {});
+
+        AlertDialog dialog = dialogBuilder.create();
+        dialog.show();
     }
 }
